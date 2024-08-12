f978133960ef213b42e9d6ca3c02662c636f6dad
amirhilal044 <amiramirhilal@gmail.com>
2024-08-12 14:56:38 +0300

    cleaned code

--- apps/prediction.py
+++ apps/prediction.py
@@ -2,10 +2,6 @@ import mlflow.pyfunc
 import pandas as pd
 from dotenv import load_dotenv
 import os
-import boto3
-from mlflow.tracking import MlflowClient
-
-from pipelines.deploy import S3_BUCKET
 
 load_dotenv()
 
@@ -13,20 +9,20 @@ load_dotenv()
 s3_installs_model_path = os.getenv("S3_INSTALLS_MODEL_PATH")
 s3_rating_model_path = os.getenv("S3_RATING_MODEL_PATH")
 aws_region = os.getenv("AWS_REGION")
+s3_bucket = os.getenv("S3_BUCKET")
 
 # Print environment variables to debug
 print(f"S3_INSTALLS_MODEL_PATH: {s3_installs_model_path}")
 print(f"S3_RATING_MODEL_PATH: {s3_rating_model_path}")
 print(f"AWS_REGION: {aws_region}")
 
-# Set the S3 endpoint URL explicitly
 # Ensure the S3 endpoint is set to the correct region
-mlflow.set_tracking_uri(f"https://s3.{aws_region}.amazonaws.com")
+mlflow.set_tracking_uri(f"https://{aws_region}.s3.amazonaws.com")
+mlflow.pyfunc.get_model_dependencies(model_uri=s3_installs_model_path)
 
-# Load models from S3 using the correct region
-model_installs = mlflow.pyfunc.load_model(f"s3://{S3_BUCKET}/{s3_installs_model_path}")
-model_rating = mlflow.pyfunc.load_model(f"s3://{S3_BUCKET}/{s3_rating_model_path}")
-
+# Load models from S3 using the correct region and path
+model_installs = mlflow.pyfunc.load_model(s3_installs_model_path)
+model_rating = mlflow.pyfunc.load_model(s3_rating_model_path)
 
 def predict_app_metrics(features):
     features_df = pd.DataFrame([features.dict()])
